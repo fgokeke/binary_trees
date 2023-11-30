b15748ce1a455366c66d43d13020f7af7f056fa9
root <okekefeargod@gmail.com>
2023-11-30 01:55:41 -0800

    1. Insert left 4

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,19 +12,17 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left_node;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_left_node = malloc(sizeof(binary_tree_t));
+	new_left_node = binary_tree_node(parent, value);
 
 	if (new_left_node == NULL)
 		return (NULL);
 
-	new_left_node->n = value;
-	new_left_node->parent = parent;
-	new_left_node->left = NULL;
-	new_left_node->right = NULL;
-	if (parent->left)
+	if (parent->left != NULL)
 	{
 		new_left_node->left = parent->left;
 		new_left_node->left->parent = new_left_node;
